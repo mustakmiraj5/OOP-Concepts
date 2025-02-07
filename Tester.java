2f71b2ce31c6c7e18ad3cd2a2fdfc06ae3e7fdb4
mustakmiraj5 <msmiraj8@gmail.com>
2025-02-07 17:30:54 +0600

    Class and Object

---
+++ Tester.java
@@ -0,0 +1,6 @@
+public class Tester {
+    public static void main(String[] args)
+    {
+        System.out.println("Testing 1, 2, 3");
+    }
+}
---
+++ class and objects/School.java
@@ -0,0 +1,6 @@
+public class School {
+    public static void main(String[] args) {
+        Student student1 = new Student();
+        System.out.println("Testing 1, 2, 3");
+    }
+}
